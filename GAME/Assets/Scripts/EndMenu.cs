d7d0fdef585dae34f53f3358f8bb66f67faeb457
Sergei K <sergeikdr1@gmail.com>
2024-04-08 10:45:53 -0400

    fixed scenes

--- GAME/Assets/Scripts/EndMenu.cs
+++ GAME/Assets/Scripts/EndMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EndMenue : MonoBehaviour
+public class EndMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
