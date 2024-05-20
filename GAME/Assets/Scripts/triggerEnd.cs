8484d345adea18567938ad96bd7c3420afdab640
Sergei K <sergeikdr1@gmail.com>
2024-05-20 15:14:13 -0400

    FINAL PRODUCT

--- GAME/Assets/Scripts/triggerEnd.cs
+++ GAME/Assets/Scripts/triggerEnd.cs
@@ -1,17 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class triggerEnd : MonoBehaviour
 {
-// Name of the scene to load
-    public string nextSceneName;
-
     // This method is called when another collider enters the trigger
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Triggered");
         // Load the next scene
-        SceneManager.LoadScene(nextSceneName);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
